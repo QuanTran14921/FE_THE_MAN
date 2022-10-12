93015e1ca32cc4c48878d9021462991a468a361a
QuanTran14921 <tquan7965@gmail.com>
2022-10-12 13:20:41 +0700

    feat: Render Post Page

--- src/pages/Post/index.tsx
+++ src/pages/Post/index.tsx
@@ -1,9 +1,24 @@
 import { Pagination } from "antd";
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useSelector } from "react-redux";
+import { getPosts, setPage } from "../../redux/slices/postSlice";
+import { RootState, useAppDispatch } from "../../redux/store";
 import styles from "./FeaturedNews.module.css";
 type Props = {}
 
 const PostPage = (props: Props) => {
+  const post = useSelector((state: RootState) => state?.post);
+  const pages = useSelector((state: RootState) => state?.post.page);
+
+  const dispatch = useAppDispatch();
+  useEffect(() => {
+    dispatch(
+      getPosts({
+        page: pages,
+        limit: 12,
+      })
+    );
+  }, [dispatch, pages]);
   return (
     <div>
     <section className={styles.news}>
@@ -14,58 +29,38 @@ const PostPage = (props: Props) => {
     <div className={styles.news__title}>
       <h2>Blog của chúng tôi</h2>
     </div>
-    <div className={styles.news__item}>
-   
-        <div className={styles.item__box}>
-          <img src="https://cf.shopee.vn/file/a9c48c5185e022563b282ab59540dd53" alt="000017" />
-          <div className={styles.detail}>
-            <h3>Tin Tức Mới 1</h3>
-            <p>áiofioaeigo</p>
-          </div>
-        </div>
-
-        <div className={styles.item__box}>
-          <img src="https://cf.shopee.vn/file/a9c48c5185e022563b282ab59540dd53" alt="000017" />
-          <div className={styles.detail}>
-            <h3>Tin Tức Mới 1</h3>
-          </div>
-        </div>
-        
-        <div className={styles.item__box}>
-          <img src="https://cf.shopee.vn/file/a9c48c5185e022563b282ab59540dd53" alt="000017" />
-          <div className={styles.detail}>
-            <h3>Tin Tức Mới 1</h3>
-          </div>
-        </div>
-
-        <div className={styles.item__box}>
-          <img src="https://cf.shopee.vn/file/a9c48c5185e022563b282ab59540dd53" alt="000017" />
-          <div className={styles.detail}>
-            <h3>Tin Tức Mới 1</h3>
-          </div>
-        </div>
-
-        <div className={styles.item__box}>
-          <img src="https://cf.shopee.vn/file/a9c48c5185e022563b282ab59540dd53" alt="000017" />
-          <div className={styles.detail}>
-            <h3>Tin Tức Mới 1</h3>
-          </div>
-        </div>
-
-        <div className={styles.item__box}>
-          <img src="https://cf.shopee.vn/file/a9c48c5185e022563b282ab59540dd53" alt="000017" />
-          <div className={styles.detail}>
-            <h3>Tin Tức Mới 1</h3>
-          </div>
-        </div>
-
-        
-    </div>
-   
     
-  </section>
+    <div className={styles.news__item}>
+    {post?.posts.Post?.map((e: any, index: any) => {
+              return (
+                <div className={styles.item__box}>
+          <img src={e.image} alt="000017" />
+          <div className={styles.detail}>
+            <h3>{e.title}</h3>
+            <p>{e.descShort}</p>
+          </div>
+        </div>
+              );
+            })}
+    </div>
 
+    
+
+        <Pagination
+          defaultCurrent={1}
+          total={post?.posts.count}
+          pageSize={12}
+          onChange={(pages) => {
+            dispatch(setPage(pages));
+          }}
+        />
+
+    
+
+  </section>
+         
   </div>
+  
   )
 }
 
